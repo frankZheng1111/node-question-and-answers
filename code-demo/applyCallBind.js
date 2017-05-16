3d07c4928c54d8064590062b71936bef48623576
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-05-16 16:43:53 +0800

    Add applyCallBind.js

---
+++ code-demo/applyCallBind.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function Person() {
+}
+Person.prototype.sayName = function (arg) { console.log(this.name, arg); }
+
+var obj = {name: 'michaelqin'}; // 注意这是一个普通对象，它不是Person的实例
+// apply
+Person.prototype.sayName.apply(obj, [1, 2, 3]);
+
+// call
+Person.prototype.sayName.call(obj, 1, 2, 3);
+
+// bind
+var sn = Person.prototype.sayName.bind(obj);
+sn(1, 2, 3); // bind需要先绑定，再执行
